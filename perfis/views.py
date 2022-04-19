6e9be26b526e5c6aa9939523e3eaf5a47ecc311b
Marcos Rian <marcos.oliveira6@estudante.ifb.edu.br>
2022-04-19 14:02:31 -0300

    Update dos módulos 2 e 3 realizados

--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+
+    perfil = Perfil()
+
+    if perfil_id == 1:
+        perfil = Perfil('Marcos Rian', 'marcos.oliveira6@estudante.ifb.edu.br', '222222', 'IFB')
+    if perfil_id == 2:
+        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
+
+
+    return render(request, 'perfil.html', {'perfil' : perfil})
